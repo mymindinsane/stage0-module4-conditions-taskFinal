b89b66f16bc2270e92ae0f6193f820c84c87dea1
mymindinsane <arturguletskiy21@gmail.com>
2024-05-16 20:05:25 +0300

    so bad

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,12 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
-        if (first != second){
             first ^= second;
             second ^= first;
             first ^= second;
-            System.out.println(first);
             System.out.println(second);
-        }
+            System.out.println(first);
     }
 }
